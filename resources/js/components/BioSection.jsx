e5cbcefb49dd6fcf1f3fccb66090465a7487487e
Daniel Pugsley <daniel.pugsley@gmail.com>
2022-08-06 17:54:23 +0100

    HOTFIX: fixed 'lightening' spelling mistake

--- resources/js/components/BioSection.jsx
+++ resources/js/components/BioSection.jsx
@@ -41,14 +41,14 @@ class AvatarTags extends React.Component {
         const angularVel = deltaAngle / deltaTime;
         const angularDir = Math.sign(angularVel);
 
-        // If the calculated angular velocity if faster than current, 
+        // If the calculated angular velocity if faster than current,
         // or in opposite direction, modify current to match.
         if (angularDir !== 0 &&
-            (angularDir !== Math.sign(this.angularVel) || 
+            (angularDir !== Math.sign(this.angularVel) ||
             Math.abs(angularVel) > Math.abs(this.angularVel))) {
             this.angularVel = angularVel;
         }
-        
+
         // Apply friction
         if (this.angularVel !== 0) {
             const currentAngularDir = Math.sign(this.angularVel);
@@ -64,7 +64,7 @@ class AvatarTags extends React.Component {
             this.angularVel,
             -data.maxAngularVel,
             data.maxAngularVel);
-            
+
         // Apply angular velocity
         this.angle -= this.angularVel * deltaTime;
         this.picRef.current.style.transform = `rotate(${rad2deg(this.angle)}deg)`;
@@ -117,7 +117,7 @@ function BioSection() {
     return (
         <section id="bio" className="bio tk-lato fade-in">
             <TextContent title={bio.name} subtitle={bio.subtitle}>
-                <p>In 2014, I didn’t know how to code. I never studied it. I woke up one morning and decided to build a video game, and so I started. The first thing I learned was how to make ‘lightening’ flash at random in a <a href="https://unity.com/" target="_blank">Unity</a> scene. One year later, after teaching myself everything I needed to know, a <a href="https://deftgames.itch.io/zead" target="_blank">demo was complete</a>. After another year and a half, I was lead programmer at a <a href="https://www.aurochdigital.com/" target="_blank">game studio</a> in my hometown. Then I built a <a href="https://blog.pugs.ly/" target="_blank">blog</a>, then an iOS app, then a <a href="https://wordpool.com/" target="_blank">language learning platform</a>, and nobody ever told me that this is what I should do. Maybe it’s a compulsion, but I can’t help but build things. It started with <a href="https://www.brio.net/products/all-products/railway/classic-railway/classic-deluxe-set" target="_blank">Brio</a>, then Lego and Knex, and now it’s HTML, CSS, JavaScript, PHP, SQL… I’m a creative builder of <em>things</em>, and code is the most versatile building block.</p>
+                <p>In 2014, I didn’t know how to code. I never studied it. I woke up one morning and decided to build a video game, and so I started. The first thing I learned was how to make ‘lightning’ flash at random in a <a href="https://unity.com/" target="_blank">Unity</a> scene. One year later, after teaching myself everything I needed to know, a <a href="https://deftgames.itch.io/zead" target="_blank">demo was complete</a>. After another year and a half, I was lead programmer at a <a href="https://www.aurochdigital.com/" target="_blank">game studio</a> in my hometown. Then I built a <a href="https://blog.pugs.ly/" target="_blank">blog</a>, then an iOS app, then a <a href="https://wordpool.com/" target="_blank">language learning platform</a>, and nobody ever told me that this is what I should do. Maybe it’s a compulsion, but I can’t help but build things. It started with <a href="https://www.brio.net/products/all-products/railway/classic-railway/classic-deluxe-set" target="_blank">Brio</a>, then Lego and Knex, and now it’s HTML, CSS, JavaScript, PHP, SQL… I’m a creative builder of <em>things</em>, and code is the most versatile building block.</p>
             </TextContent>
             <Avatar/>
             <div></div> {/* improves flex layout */}
