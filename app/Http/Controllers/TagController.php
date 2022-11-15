4ecff526aa933a44387efa80a471cf071ca680f7
Daniel Pugsley <daniel.pugsley@gmail.com>
2022-11-15 16:01:01 +0000

    don't show tags for hidden projects

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -10,6 +10,8 @@ class TagController extends Controller
     public function list(Request $request)
     {
         return Tag::join('project_tag', 'project_tag.tag_id', '=', 'tags.id')
+                ->join('projects', 'projects.id', '=', 'project_tag.project_id')
+                ->where('projects.is_hidden', '=', 0)
                 ->distinct()
                 ->orderBy('tags.name')
                 ->get(['tags.id', 'tags.name']);
